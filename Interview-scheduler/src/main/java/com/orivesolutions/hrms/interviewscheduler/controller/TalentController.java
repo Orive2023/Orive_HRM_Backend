5b2959fce36cfc9d8ca5176f37e231e9a7ac2574
JMonalisa-98 <you@example.commonalisarout1999@gmail.com>
2023-12-16 11:35:24 +0530

    Monlisas changes

--- Interview-scheduler/src/main/java/com/orivesolutions/hrms/interviewscheduler/controller/TalentController.java
+++ Interview-scheduler/src/main/java/com/orivesolutions/hrms/interviewscheduler/controller/TalentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RequestMapping("/talents")
 @RestController
+@CrossOrigin(origins = "*")
 public class TalentController {
 
     @Autowired
--- Interview-scheduler/src/main/java/com/orivesolutions/hrms/interviewscheduler/controller/UserController.java
+++ Interview-scheduler/src/main/java/com/orivesolutions/hrms/interviewscheduler/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RequestMapping("/users")
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
